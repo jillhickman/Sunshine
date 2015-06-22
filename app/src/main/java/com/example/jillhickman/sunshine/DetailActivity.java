f653e184bacb2d202315ec9cf22f5f8dea5a459e
Jill Hickman <jillhickman@gmail.com>
2015-06-22 16:38:38 -0600

    Created settingsActivity and launched it

--- app/src/main/java/com/example/jillhickman/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/jillhickman/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.jillhickman.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -39,6 +40,8 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //intent for settingsActivity
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
