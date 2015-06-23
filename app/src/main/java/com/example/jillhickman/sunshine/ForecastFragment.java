930ca32bd5b778dcdc4f19f70797b240ecae7719
Jill Hickman <jillhickman@gmail.com>
2015-06-22 22:52:24 -0600

    Used the location prefernce  when refresh button tapped

--- app/src/main/java/com/example/jillhickman/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/jillhickman/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.jillhickman.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -65,7 +67,11 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id==R.id.action_refresh){
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("84042");
+            //When refresh button pressed, check to see if a location is stored in Preference,
+            //otherwise use the default.
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = preferences.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
